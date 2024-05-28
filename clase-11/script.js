856bf05c7e37580f758b0e68e418503df51b6f31
Kintana <artrod27@gmail.com>
2024-05-27 23:21:56 -0300

    clase-9

---
+++ clase-11/script.js
@@ -0,0 +1,29 @@
+
+/* Ejercicio 1 */
+
+/* let edad = prompt("Ingresa tu edad");
+
+if (edad == 0){
+    console.log("Numero invalido");
+}
+else if (edad <= 13){
+    console.log ("Eres un niño");
+}
+else if (edad >= 13 && edad <= 17 ){
+    console.log ("Sos un adolescente");
+}
+else if (edad >= 18 && edad <= 64){
+    console.log ("Sos un adulto");
+}
+else{
+    console.log("Sos una persona mayor");
+}
+
+/* Ejercicio 2 */
+
+let calificacion = prompt("Ingresa una calificación numerica")
+
+if (calificacion >= 90 && calificacion <=100 ) {
+    console.log("A")
+} 
+else if ()
